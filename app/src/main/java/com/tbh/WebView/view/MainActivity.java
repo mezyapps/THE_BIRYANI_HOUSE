d1c63daaf5461e259e2fa5c5fbdb79c481e1195a
mezyapps <mezyapps@gmail.com>
2019-08-23 18:08:52 +0530

    2 NJ commit 2019

--- app/src/main/java/com/tbh/WebView/view/MainActivity.java
+++ app/src/main/java/com/tbh/WebView/view/MainActivity.java
@@ -2,15 +2,66 @@ package com.tbh.WebView.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.tbh.WebView.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_order_now;
+    private boolean doubleBackToExitPressedOnce = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        find_View_IDs();
+        events();
     }
+
+    private void find_View_IDs() {
+        btn_order_now=findViewById(R.id.btn_order_now);
+    }
+
+    private void events() {
+        btn_order_now.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,OrderWebViewActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        doubleBackPressLogic();
+    }
+
+    // ============ End Double tab back press logic =================
+    private void doubleBackPressLogic() {
+        if (!doubleBackToExitPressedOnce) {
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Please click back again to exit !!", Toast.LENGTH_SHORT).show();
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 1000);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+    }
+
 }
--- app/src/main/java/com/tbh/WebView/view/OrderWebViewActivity.java
+++ app/src/main/java/com/tbh/WebView/view/OrderWebViewActivity.java
@@ -2,15 +2,70 @@ package com.tbh.WebView.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
 
 import com.tbh.WebView.R;
+import com.tbh.WebView.utils.ShowProgress;
 
 public class OrderWebViewActivity extends AppCompatActivity {
 
+    private WebView webViewTBH;
+    private ShowProgress showProgress;
+    private ImageView iv_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_web_view);
+
+        find_View_IDs();
+        events();
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void find_View_IDs() {
+        showProgress=new ShowProgress(OrderWebViewActivity.this);
+        webViewTBH=findViewById(R.id.webViewTBH);
+        iv_back=findViewById(R.id.iv_back);
+        webViewTBH.getSettings().setLoadsImagesAutomatically(true);
+        webViewTBH.getSettings().setJavaScriptEnabled(true);
+        webViewTBH.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webViewTBH.loadUrl("https://thebiryanihouse.petpooja.com/");
+        webViewTBH.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon)
+            {
+             //   showProgress.showDialog();
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+               // showProgress.dismissDialog();
+            }
+        });
+
+    }
+
+    private void events() {
+        iv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(OrderWebViewActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
     }
 }
